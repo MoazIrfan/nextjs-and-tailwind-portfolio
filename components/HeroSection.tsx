49c5d5a678cc7c6922c05e4874db2bf219226c92
MoazIrfan <irfanmoaz@gmail.com>
2023-11-17 15:18:28 +0500

    📦 NEW: Add latest project links

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -20,9 +20,12 @@ const HeroSection = () => {
     <section id="home" className="mx-auto max-w-3xl  md:max-w-6xl">
       <div className="flex flex-col items-center justify-center animate-fadeIn animation-delay-2 my-8 py-16 sm:py-32  md:flex-row md:space-x-4 md:text-left">
 
-        <div className="pb-8 md:pr-8 md:w-1/2">
-          <p style={archivo.style}  className="mt-4 mb-6 text-36 leading-48 tracking-.8">
+        <div style={archivo.style} className="pb-8 md:pr-8 md:w-1/2">
+          <p className="mt-4 mb-6 text-36 leading-48 tracking-.8">
             I&#39;m Moaz Irfan, a Web Engineer. I&#39;m focused on creating scalable, easy-to-maintain, and performant web applications.
+          </p>
+          <p className="text-lg tracking-wide font-thin opacity-75">
+            Recent Projects: <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://github.com/MoazIrfan/react-tailwind-app">react-tailwind-app</a>, <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://github.com/MoazIrfan/next-blog-app">next-blog-app</a>, <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://github.com/MoazIrfan/cwc-cli">cwc-cli</a>, <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://github.com/MoazIrfan/NestJS-Authentication-Login-Signup">NestJS auth</a>, <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://github.com/MoazIrfan/JavaScript-code-snippets">JavaScript code snippets</a>
           </p>
         </div>
 
